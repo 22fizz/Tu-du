565f62944003ba7c4fe1da8defec03a270f8dbc9
Strahinja Ljaljevic <STRAHINJA@aleksandars-macbook-air.local>
2018-12-13 16:46:08 +0100

    Convert to MVC design pattern.

---
+++ Tu-du/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Tu-du
+//
+//  Created by Strahinja Ljaljević on 12/13/18.
+//  Copyright © 2018 Strahinja Ljaljević. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
